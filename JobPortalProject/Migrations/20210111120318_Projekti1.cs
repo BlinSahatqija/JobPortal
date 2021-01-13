676d6397892dacd35a739c38e19abb2e8bf07a6e
Genat Hadri <gh43836@ubt-uni.net>
2021-01-13 10:15:21 +0100

    appsettings.json dhe scaffold

---
+++ JobPortalProject/Migrations/20210111120318_Projekti1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JobPortalProject.Migrations
+{
+    public partial class Projekti1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Jobs",
+                keyColumn: "Id",
+                keyValue: 123,
+                column: "DateIn",
+                value: new DateTime(2021, 1, 11, 13, 3, 18, 94, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Jobs",
+                keyColumn: "Id",
+                keyValue: 124,
+                column: "DateIn",
+                value: new DateTime(2021, 1, 11, 13, 3, 18, 96, DateTimeKind.Local));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Jobs",
+                keyColumn: "Id",
+                keyValue: 123,
+                column: "DateIn",
+                value: new DateTime(2020, 1, 28, 14, 2, 15, 31, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Jobs",
+                keyColumn: "Id",
+                keyValue: 124,
+                column: "DateIn",
+                value: new DateTime(2020, 1, 28, 14, 2, 15, 34, DateTimeKind.Local));
+        }
+    }
+}
